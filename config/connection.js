19a857617ed68c18e7d47107da1adcaff594fe12
Dennys Joseph <dennysjoseph1999@gmail.com>
2022-09-02 17:22:56 +0530

    ready for hosting

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,19 @@
-var MongoClient=require('mongodb').MongoClient
-const state={
-    db:null
-}
-module.exports.connect=function(done){
-    const url='mongodb://localhost:27017'
-    const dbname='shopping'
-    MongoClient.connect(url,(err,data)=>{
-         if(err) return done(err)
-         state.db=data.db(dbname)
-         done()
-    })
+require("dotenv").config();
+var MongoClient = require("mongodb").MongoClient;
+const state = {
+  db: null, 
+}; 
+module.exports.connect = function (done) {
+  const url = `mongodb+srv://dennys:${process.env.MONGO_PASSWORD}@cluster0.guojgqv.mongodb.net/?retryWrites=true&w=majority`;
+  //const url='mongodb://localhost:27017'
+  const dbname = "z-fashions";
+  MongoClient.connect(url, (err, data) => {
+    if (err) return done(err);
+    state.db = data.db(dbname);
+    done();
+  }); 
+};
 
-}
-
-module.exports.get=function(){
-    return state.db
-}
+module.exports.get = function () {
+  return state.db;
+};
