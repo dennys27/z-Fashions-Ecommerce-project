c366e48afb8b81261741a3b4b83d8fefdc177719
Dennys Joseph <dennysjoseph1999@gmail.com>
2022-09-03 15:16:26 +0530

    testing

--- config/connection.js
+++ config/connection.js
@@ -3,15 +3,15 @@ var MongoClient = require("mongodb").MongoClient;
 const state = {
   db: null, 
 }; 
-module.exports.connect = function (done) {
+module.exports.connect =  function  (done) {
   const url = `mongodb+srv://dennys:${process.env.MONGO_PASSWORD}@cluster0.guojgqv.mongodb.net/?retryWrites=true&w=majority`;
   //const url='mongodb://localhost:27017'
   const dbname = "z-fashions";
-  MongoClient.connect(url, (err, data) => {
+  MongoClient.connect(url, async (err, data) => {
     if (err) return done(err);
-    state.db = data.db(dbname);
+    state.db = await data.db(dbname);
     done();
-  }); 
+  });  
 };
 
 module.exports.get = function () {
