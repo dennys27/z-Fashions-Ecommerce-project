cb8985c2a9b99b345de3b8b7e16ec9c2e6c55a8d
Dennys Joseph <dennysjoseph1999@gmail.com>
2022-09-06 15:40:38 +0530

    three

--- helpers/user-helpers.js
+++ helpers/user-helpers.js
@@ -559,8 +559,8 @@ paypal.configure({
               payment_method: "paypal",
             },
             redirect_urls: {
-              return_url: "http://localhost:3000/success",
-              cancel_url: "http://localhost:3000/cancel",
+              return_url: "https://www.z-fashions.gq/success",
+              cancel_url: "https://www.z-fashions.gq/cancel",
             },
             transactions: [
               {
